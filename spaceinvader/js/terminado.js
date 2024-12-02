bd5cfd96ec39036a55c9719d0c33ff5535bf9054
sacacorchosupremo <ipena252@ieszaidinvergeles.org>
2024-12-02 10:31:17 +0100

    Add files via upload

---
+++ spaceinvader/js/terminado.js
@@ -0,0 +1,14 @@
+var Terminado = {
+    preload:function(){
+
+    },
+    create: function(){
+        juego.stage.backgroundColor="#990000";
+        if(confirm("¿Desea reiniciar el juego?")){
+            juego.state.start('Juego');
+        }
+    },
+    update: function(){
+        
+    }
+};
